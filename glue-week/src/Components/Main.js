ef4e2c9aaccb657cc27db1905bed9bda685e0542
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-05-18 12:57:20 -0500

    js

---
+++ glue-week/src/Components/Main.js
@@ -0,0 +1,8 @@
+import data from '../data.json';
+import Posts from './Posts';
+
+function Main () {
+
+}
+
+export default Main
---
+++ glue-week/src/Components/Posts.js
@@ -0,0 +1,9 @@
+import react from 'react';
+import {useState} from 'react';
+
+
+function Posts () {
+
+}
+
+export default Posts
